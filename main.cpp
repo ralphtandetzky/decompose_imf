391e5bcdd565d95fd26a45cc976510aa37771241
Ralph Tandetzky <ralph.tandetzky@gmail.com>
2014-02-15 22:30:13 +0100

    Removed commented out code.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "gui_main_window.h"
 #include "qt_utils/exception_handling_application.h"
 
-
 int main(int argc, char *argv[])
 {
     qu::ExceptionHandlingApplication a(argc, argv);
@@ -10,39 +9,3 @@ int main(int argc, char *argv[])
     
     return a.exec();
 }
-
-/*
-#include "cpp_utils/cow_tree.h"
-#include <iostream>
-
-int main()
-try
-{
-    cu::CowMap<int,int> t;
-
-    static const int N = 100;
-
-    for ( int i = 0; i < N; ++i )
-        t.insert( i, N-i );
-
-    for ( int i = 0; i < N; ++i )
-        t.modify( i, []( int & i ){ i = i*i; } );
-
-    for ( int i = N/2; i < N; ++i )
-        t.remove( i );
-
-    t.modifyAll( [](int,int&i){++i;} );
-
-    t.readAll( [](int key, int val)
-    {
-        std::cout
-            << "key = "    << key
-            << ",\tval = " << val
-            << std::endl;
-    });
-}
-catch( std::exception & e )
-{
-    std::cout << e.what() << std::endl;
-}
-*/
